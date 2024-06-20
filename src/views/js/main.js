8b394d447ef5fe732bd49c581e10650711a8ee1b
Richard Antunis <richardantunis4@gmail.com>
2024-06-19 22:04:42 -0300

    feat: adding post request

---
+++ src/views/js/main.js
@@ -0,0 +1,36 @@
+const btnSubmit = document.querySelector('#submit')
+
+const form = document.querySelector('#form')
+
+btnSubmit.onclick = function (e) {
+  e.preventDefault()
+
+  const name = form.name.value
+  const description = form.description.value
+  const url = form.url.value
+  const image = form.image.value
+
+  fetch('http://localhost:8080/api/projects', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: name,
+      description: description,
+      url: url,
+      image: image,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.message === 'Success. Document created') {
+        alert('Formulario cadastrado com sucesso')
+      } else {
+        alert('Deu erro')
+      }
+    })
+    .catch((error) => {
+      console.log('Erro do catch: ', error)
+    })
+}
