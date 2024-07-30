d9602d30fdde8d24d4ad06aae02730e7cf6589f0
Richard Antunis <richardantunis4@gmail.com>
2024-07-30 16:50:13 -0300

    feat: adding delete button

--- src/views/js/projects.js
+++ src/views/js/projects.js
@@ -1,6 +1,10 @@
-const projectsList = document.querySelector('#projects-list')
+document.addEventListener('DOMContentLoaded', function () {
+  getProjectList()
+})
 
-window.addEventListener('load', () => {
+function getProjectList() {
+  const projectsList = document.querySelector('#projects-list')
+
   fetch('http://localhost:8080/api/projects', { method: 'GET' })
     .then((res) => {
       res.json().then((data) => {
@@ -20,11 +24,38 @@ window.addEventListener('load', () => {
           <li>
             <a href='${project.url}'>GitHub</a>
           </li>
-        </ul>`
+        </ul>
+        
+        <a href="#" class="removeBtn" id="deleteBtn" data-id="${project._id}">Delete Project</a>
+        `
           )
           .join('')
         projectsList.innerHTML = projectsHTML
+        deleteProjects()
       })
     })
     .catch((error) => console.error('Error:', error))
-})
+}
+
+function deleteProjects() {
+  const removeBtn = document.querySelectorAll('#deleteBtn')
+
+  removeBtn.forEach((button) => {
+    button.onclick = function (e) {
+      e.preventDefault()
+
+      const id = this.dataset.id
+      console.log(id)
+      fetch(`http://localhost:8080/api/projects/${id}`, {
+        method: 'DELETE',
+      }).then((res) => {
+        if (res.status === 204) {
+          alert('Projeto excluido')
+          getProjectList()
+        } else {
+          alert('Deu erro')
+        }
+      })
+    }
+  })
+}
