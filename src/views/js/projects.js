a980aaeec8ec9bc89eb272f9a3a53887a6d71b73
Richard Antunis <richardantunis4@gmail.com>
2024-08-07 22:03:05 -0300

    feat: adding update button

--- src/views/js/projects.js
+++ src/views/js/projects.js
@@ -1,3 +1,5 @@
+const updateForm = document.querySelector('#updtForm')
+
 document.addEventListener('DOMContentLoaded', function () {
   getProjectList()
 })
@@ -27,11 +29,13 @@ function getProjectList() {
         </ul>
         
         <a href="#" class="removeBtn" id="deleteBtn" data-id="${project._id}">Delete Project</a>
+        <a href="#" class="updateBtn" id="updateBtn" data-id="${project._id}">Update Project</a>
         `
           )
           .join('')
         projectsList.innerHTML = projectsHTML
         deleteProjects()
+        setUpdateBtns()
       })
     })
     .catch((error) => console.error('Error:', error))
@@ -58,4 +62,91 @@ function deleteProjects() {
       })
     }
   })
+}
+
+function setUpdateBtns() {
+  const updateBtn = document.querySelectorAll('#updateBtn')
+
+  updateBtn.forEach((button) => {
+    button.onclick = function (e) {
+      e.preventDefault()
+
+      const id = this.dataset.id
+      console.log(id)
+      console.log('passou por aqui')
+
+      openForm(id)
+    }
+  })
+}
+
+async function openForm(id) {
+  const btnSave = document.querySelector('#saveBtn')
+  const btnCancel = document.querySelector('#cancelBtn')
+
+  updateForm.classList.remove('hidden')
+  try {
+    const res = await fetch(`http://localhost:8080/api/projects/${id}`, {
+      method: 'GET',
+    })
+    const data = await res.json()
+
+    const project = data[0]
+
+    let img = (document.getElementById('img').value = project.image)
+    let name = (document.getElementById('name').value = project.name)
+    let desc = (document.getElementById('description').value =
+      project.description)
+    let url = (document.getElementById('url').value = project.url)
+
+    btnSave.onclick = function () {
+      let newImg = document.getElementById('img').value
+      let newName = document.getElementById('name').value
+      let newDesc = document.getElementById('description').value
+      let newUrl = document.getElementById('url').value
+
+      name = newName
+      img = newImg
+      desc = newDesc
+      url = newUrl
+      updateRequest(name, desc, url, img, id)
+    }
+
+    btnCancel.onclick = function () {
+      name = ''
+      img = ''
+      desc = ''
+      url = ''
+
+      updateForm.classList.add('hidden')
+    }
+  } catch (error) {
+    console.error('Error:', error)
+  }
+}
+
+function updateRequest(name, desc, url, img, id) {
+  console.log(id, name, desc, url, img)
+
+  fetch(`http://localhost:8080/api/projects/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: name,
+      description: desc,
+      url: url,
+      image: img,
+    }),
+  })
+    .then((res) => {
+      res.json().then((data) => {
+        if (data.message === 'Success. Document updated') {
+          alert('Projeto Atualizado')
+          updateForm.classList.add('hidden')
+        }
+      })
+    })
+    .catch((error) => console.log(error))
 }
