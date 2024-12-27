aa91145992f4839778d93b3b77d2d4c053534e4b
Richard Antunis <richardantunis4@gmail.com>
2024-12-27 17:15:02 -0300

    feat: adding form modal in project's page

--- src/views/index.jsx
+++ src/views/index.jsx
@@ -1,10 +1,77 @@
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 import { Button } from '@/components/ui/button'
+import { Textarea } from '@/components/ui/textarea'
 
 export default function Home() {
   return (
     <>
-      <h1 className="text-slate-200"> Add your project here</h1>
-      <Button> Shadcn test</Button>
+      <Dialog className="border-2">
+        <DialogTrigger asChild>
+          <Button
+            variant="outline"
+            className="border-gray-600 hover:bg-gray-600 transition duration-300"
+          >
+            Add your project here
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Add Projects</DialogTitle>
+            <DialogDescription>
+              Show us everything about your best projects!
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Name
+              </Label>
+              <Input id="name" className="col-span-3" />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="image" className="text-right">
+                Image
+              </Label>
+              <Input type="file" id="image" className="col-span-3 p-2" />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="description" className="text-right">
+                Description
+              </Label>
+              <Textarea id="description" className="col-span-3" />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="url" className="text-right">
+                Url
+              </Label>
+              <Input type="url" id="url" className="col-span-3" />
+            </div>
+          </div>
+          <DialogFooter>
+            <DialogClose>
+              <Button type="button" variant="outline">
+                Cancel
+              </Button>
+            </DialogClose>
+
+            <Button type="submit" className="text-black">
+              Save project
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </>
   )
 }
