90a354a3606bfa90f691f11e3ffbe50025f71c4a
Richard Antunis <richardantunis4@gmail.com>
2024-12-28 16:15:01 -0300

    feat: adding Header component

---
+++ src/reactComponents/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <>
+      <div className="flex justify-center mb-5">
+        <h1 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+          Project Manager
+        </h1>
+      </div>
+    </>
+  )
+}
